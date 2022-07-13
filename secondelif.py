cc83c8030bd526641bf6c945a8f2cce6a8536be2
Pratyush_Chakraborty <mrphenomgem@gmail.com>
2022-07-13 15:10:49 +0530

    second commit

--- secondelif.py
+++ secondelif.py
@@ -5,3 +5,7 @@ elif age < 18:
     print("your age is lesser than 18")
 else:
     print("your age is equal to 18")
+#new addition
+age = 9
+if age <= 18:
+    print("You are not eligible for voting")
